6507c6b44fabc667e05d97b45c1358da2271a216
myle-truong <56028880+myle-truong@users.noreply.github.com>
2020-01-16 16:34:31 +0100

    fix current time

--- app.js
+++ app.js
@@ -10,37 +10,16 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     let today = new Date();
-    let currentDay = today.getDay();
-    let day = " ";
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    };
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Woesnday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
+    let day = today.toLocaleDateString('en-US', options);
 
-        default:
-            console.log('Error: current day is equal to:' + currentday)
-
-    }
     res.render('list', { day: day });
+
 });
 
 app.listen(3000, function() {
