b6fed38e8379f042d9b1b6515ebc8ca71d06a6ac
myle-truong <56028880+myle-truong@users.noreply.github.com>
2020-01-21 11:51:20 +0100

    Add css style

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const ejs = require('ejs');
 
 const app = express();
 
-let item = "";
-//ejs documents
+let items = ["Shopping", "Studying", "Enjoying"];
+let workItems = [];
+
+//ejs documents: link: github
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+//embed public file 
+app.use(express.static("public"));
 
 app.get('/', (req, res) => {
 
@@ -23,16 +27,29 @@ app.get('/', (req, res) => {
 
     let day = today.toLocaleDateString('en-US', options);
 
-    res.render('list', { thatDay: day, newListItem: item });
+    res.render('list', { listTitle: day, newListItems: items });
 
 });
 
 app.post('/', function(req, res) {
+
     let item = req.body.newItem;
 
+    items.push(item);
+
     res.redirect('/');
+});
 
 
+app.get('/work', (req, res) => {
+    // res.render('list', { listTitle: 'Work List', newListItems: 'workItems' });
+    res.render('list', { listTitle: 'Work List', newListItems: workItems });
+});
+
+app.post('/work', (req, res) => {
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect('/')
 });
 
 
